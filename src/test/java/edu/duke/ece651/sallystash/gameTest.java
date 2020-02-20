bc3c182b34d0183fd9f8308951093d24d29f7da3
km480 <km480@duke.edu>
2020-02-20 01:34:44 -0500

    scanner_fix

---
+++ src/test/java/edu/duke/ece651/sallystash/gameTest.java
@@ -0,0 +1,13 @@
+package edu.duke.ece651.sallystash;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class gameTest {
+  @Test
+  public void test_() {
+    
+  }
+
+}
